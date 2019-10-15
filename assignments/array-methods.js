e063f9b0cbe9350f0fe471de8aa5cf14cb077af0
Taylor Cool <taycool@gmail.com>
2019-10-15 13:18:39 -0700

    updated array-methods

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -57,7 +57,7 @@ const runners = [
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
-let fullNames = [];
+let fullNames = runners.map(a => a.first_name, arr.last_name);
 console.log(fullNames);
 
 // ==== Challenge 2: Use .map() ====
