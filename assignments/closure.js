f38e2d93897ddc449b34db1db8f19b8e96ea4045
Taylor Cool <taycool@gmail.com>
2019-10-16 07:59:46 -0700

    updated closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,6 +4,14 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+const itemFind = () => {
+  let item = "phone";
+  const locate = (thing) => {
+    console.log(`Your ${thing} is in the kitchen`)
+  }
+  locate(item);
+}
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
